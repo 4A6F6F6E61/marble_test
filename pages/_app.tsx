e08d0183635c0a4f54c8da3171873aa3eb86e506
Zockedidock <zockedidockhd@gmail.com>
2022-08-12 12:48:47 +0200

    dont use system color theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 
 const theme = extendTheme({
 	config: {
-		useSystemColorMode: true,
+		//useSystemColorMode: true,
 		initialColorMode: "light",
 	},
 })
